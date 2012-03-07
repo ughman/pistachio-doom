cfa45c2ef1e9538b6d0b521493fac82ea5050454
U-POPPY\Administrator <Administrator@poppy.(none)>
2012-03-07 19:35:28 +0000

    Add missing #includes
    + Add <string.h> and <limits.h> everywhere
    + Add <cstdlib> in Math.cpp
    + Change <math.h> to <cmath> in Math.cpp

--- src/LangExt.hpp
+++ src/LangExt.hpp
@@ -1,6 +1,9 @@
 #ifndef LangExt_hpp
 #define LangExt_hpp
 
+#include <string.h>
+#include <limits.h>
+
 #define using(decl) do { decl; try
 #define end_using(name) catch (...) { delete name; throw; } } while (false)
 #define end_using_array(name) catch (...) { delete [] name; throw; } } while (false)
