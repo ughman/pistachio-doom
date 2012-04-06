75cefbe565a7d6587dc2b3fb9b2e152feaa9fa56
U-CHEEK\Administrator <Administrator@cheek.(none)>
2012-04-06 15:29:32 +0000

    Add message for unhandled exceptions
    + Replace i_main.c
    + Add exception handler around call to D_DoomMain

---
+++ src/i_main.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+
+#include "Exception.hpp"
+
+extern "C"
+{
+#include "m_argv.h"
+#include "d_main.h"
+}
+
+int main(int argc,char **argv)
+{
+	myargc = argc;
+	myargv = argv;
+
+	try
+	{
+		D_DoomMain();
+	}
+	catch (Exception &Error)
+	{
+		fprintf(stderr,"Unhandled Exception: %s\n",Error.Message());
+		fflush(stderr);
+		return 0;
+	}
+	return 0;
+}
