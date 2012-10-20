cef90582341b66a60576229d2100cbb65dd687ff
U-VAMOOLA\Administrator <Administrator@vamoola.(none)>
2012-10-20 05:57:03 +0100

    Only catch unhandled exceptions in release builds
    + #define NDEBUG in release builds

--- src/i_main.cpp
+++ src/i_main.cpp
@@ -13,15 +13,19 @@ int main(int argc,char **argv)
 	myargc = argc;
 	myargv = argv;
 
+#ifdef NDEBUG
 	try
+#endif
 	{
 		D_DoomMain();
 	}
+#ifdef NDEBUG
 	catch (Exception &Error)
 	{
 		fprintf(stderr,"Unhandled Exception: %s\n",Error.Message());
 		fflush(stderr);
 		return 0;
 	}
+#endif
 	return 0;
 }
